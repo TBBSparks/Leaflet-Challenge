1b107542170d11577ae5eadc88a9946571447dd7
Tina Sparks <61213724+TBBSparks@users.noreply.github.com>
2020-08-22 23:04:56 -0500

    Update logic.js

--- js/logic.js
+++ js/logic.js
@@ -159,7 +159,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geo
     function(platedata) {
  
       L.geoJson(platedata, {
-        color: "DarkBlue",
+        color: "Azure",
         weight: 3
       })
       .addTo(Plates);
